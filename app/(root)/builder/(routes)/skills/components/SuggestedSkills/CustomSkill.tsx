df4c44de32dc1e2f750bf7b2f82a0677e59d45cb
devMukulSingh <mukulsingh2276@gmail.com>
2024-03-29 17:20:31 +0530

    renamed img

--- app/(root)/builder/(routes)/skills/components/SuggestedSkills/CustomSkill.tsx
+++ app/(root)/builder/(routes)/skills/components/SuggestedSkills/CustomSkill.tsx
@@ -34,7 +34,7 @@ const CustomSkill = () => {
     resolver: zodResolver(schema),
   });
   const skillFromState = useAppSelector(
-    (state) => state.persistedReducer.technicalSkills
+    (state) => state.persistedReducer.technicalSkills,
   );
 
   const onSubmit = (data: formSchema, e?: React.BaseSyntheticEvent) => {
@@ -58,7 +58,7 @@ const CustomSkill = () => {
     <Form {...form}>
       <form
         onSubmit={form.handleSubmit(
-          (data: formSchema, e?: React.BaseSyntheticEvent) => onSubmit(data, e)
+          (data: formSchema, e?: React.BaseSyntheticEvent) => onSubmit(data, e),
         )}
         className="cols-span-1 sm:col-span-2"
       >
